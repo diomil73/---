5b7c56a0979186fb1da1278f637c736ef5ab5ede
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:10:37 +0300

    Implement data loading from Google Forms
    Add function to load data from Google Forms and print statistics

--- python/import_data.py
+++ python/import_data.py
@@ -1,1 +1,23 @@
+"""
+ΣΑΠ-ΦΘ
+Import Google Forms data
+"""
 
+import pandas as pd
+
+from config import GOOGLE_EXPORT
+
+
+def load_data():
+
+    df = pd.read_excel(GOOGLE_EXPORT)
+
+    print(f"Εγγραφές : {len(df)}")
+    print(f"Πεδία    : {len(df.columns)}")
+
+    return df
+
+
+if __name__ == "__main__":
+
+    load_data()
