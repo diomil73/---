c584e25256bc87c63deb89fc1a9b649993f756ab
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:18:50 +0300

    Implement Likert scale and conversion function
    Added a Likert scale dictionary and a conversion function.

--- python/likert_engine.py
+++ python/likert_engine.py
@@ -1,1 +1,16 @@
+"""
+Likert Engine
+"""
 
+LIKERT = {
+    "Πάρα πολύ": 5,
+    "Πολύ": 4,
+    "Μέτρια": 3,
+    "Λίγο": 2,
+    "Καθόλου": 1,
+}
+
+
+def convert(series):
+
+    return series.replace(LIKERT)
