f6379a407dbfced738b085565173ebb33f63596d
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:20:16 +0300

    Implement MASTER Builder functionality

--- python/master_builder.py
+++ python/master_builder.py
@@ -1,1 +1,22 @@
+"""
+MASTER Builder
+"""
 
+from import_data import load_data
+from question_classifier import classify_questions
+
+
+def build():
+
+    df = load_data()
+
+    metadata = classify_questions(df.columns)
+
+    print(metadata)
+
+    return metadata
+
+
+if __name__ == "__main__":
+
+    build()
