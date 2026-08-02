7d79ef4073bf19f4fc7bc60e86dbeee6c020eb71
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:03:36 +0300

    Add main entry point for the application

--- python/run.py
+++ python/run.py
@@ -1,1 +1,22 @@
+"""
+ΣΑΠ-ΦΘ
+Main Entry Point
+"""
 
+from generator import create_master
+
+
+def main():
+
+    print("=" * 60)
+    print("ΣΑΠ-ΦΘ")
+    print("Σύστημα Αξιολόγησης και Ποιότητας Τμήματος Φυσικοθεραπείας")
+    print("=" * 60)
+
+    create_master()
+
+    print("\nΟλοκληρώθηκε επιτυχώς.")
+
+
+if __name__ == "__main__":
+    main()
