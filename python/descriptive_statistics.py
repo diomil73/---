826bffc147777cf181ae072153df99bbaea1bf8f
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:40:24 +0300

    Add describe function for descriptive statistics

--- python/descriptive_statistics.py
+++ python/descriptive_statistics.py
@@ -1,1 +1,14 @@
+"""
+Descriptive Statistics
+"""
 
+from scoring_engine import build_scored_dataset
+
+
+def describe():
+
+    df = build_scored_dataset()
+
+    numeric = df.select_dtypes("number")
+
+    return numeric.describe().T
