e0b02c8d0edd34cf8e231c8ed25033c8011649c2
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:35:01 +0300

    Add metadata export functionality to export_metadata.py

--- python/export_metadata.py
+++ python/export_metadata.py
@@ -1,1 +1,17 @@
+"""
+Export Metadata
+"""
 
+from metadata_engine import build_metadata
+from config import DOCS_DIR
+
+DOCS_DIR.mkdir(exist_ok=True)
+
+meta = build_metadata()
+
+meta.to_excel(
+    DOCS_DIR / "Question_Metadata.xlsx",
+    index=False,
+)
+
+print("Metadata exported.")
