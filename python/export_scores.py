9ff331a467c27f9a1a66f706a68b5e2a9386e8b1
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:38:34 +0300

    Add export functionality for section scores

--- python/export_scores.py
+++ python/export_scores.py
@@ -1,1 +1,18 @@
+"""
+Export Scores
+"""
 
+from config import REPORTS_DIR
+from section_scores import section_means
+
+REPORTS_DIR.mkdir(exist_ok=True)
+
+section_means().to_excel(
+
+    REPORTS_DIR / "Section_Scores.xlsx",
+
+    index=False
+
+)
+
+print("Export completed.")
