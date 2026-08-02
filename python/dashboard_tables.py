bd8a2b7b093cd234df5e46e7b61c8bc676725a63
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:41:59 +0300

    Add dashboard_data function for IQI and sections

--- python/dashboard_tables.py
+++ python/dashboard_tables.py
@@ -1,1 +1,19 @@
+"""
+Dashboard Tables
+"""
 
+from section_scores import section_means
+from overall_iqi import calculate_iqi
+
+
+def dashboard_data():
+
+    sections = section_means()
+
+    return {
+
+        "IQI": calculate_iqi(),
+
+        "Sections": sections
+
+    }
