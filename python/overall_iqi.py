548bda665109468d2370dd158d0a580632b123f1
diomil73 <milidakisdionissis@gmail.com>
2026-08-02 21:37:47 +0300

    Add IQI calculation function to overall_iqi.py

--- python/overall_iqi.py
+++ python/overall_iqi.py
@@ -1,1 +1,18 @@
+"""
+ΣΑΠ-ΦΘ
+IQI
+"""
 
+from section_scores import section_means
+
+
+def calculate_iqi():
+
+    sections = section_means()
+
+    return round(sections["Mean"].mean(), 2)
+
+
+if __name__ == "__main__":
+
+    print("IQI =", calculate_iqi())
